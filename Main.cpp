89ce650ebfef7720f1282e6b11db4b27f5a7d9ff
student-Ian <65768926+student-Ian@users.noreply.github.com>
2022-12-15 17:54:38 +0800

    Update Main.cpp
    Moved player size to mainChar.hpp
    Added a collideWithWall function

--- Main.cpp
+++ Main.cpp
@@ -3,15 +3,22 @@
 #include "mainChar.hpp"
 using namespace sf;
 
+// bool collideWithWall(Image image, int x, int y);
+
 int main()
 {
 
-	RenderWindow window(VideoMode(600, 400), "title");
+	RenderWindow window(VideoMode(900, 900), "title", Style::Titlebar | Style::Close);
+	Texture t;
+	t.loadFromFile("C:/Users/olaba/Downloads/1671090937051.jpg");
+	Sprite backGround(t);
+
 	Event event;
 
-	Entity player(window.getSize().y / 15);
+	Entity player;
 	Vector2f speed(0, 0);
 	Clock clock;
+
 	while (window.isOpen())
 	{
 		while (window.pollEvent(event))
@@ -24,6 +31,7 @@ int main()
 				player.processEvents(event.key.code, false);
 		}
 		window.clear();
+		window.draw(backGround);
 		Time elapsed = clock.restart();
 		player.updateMotion(elapsed);
 		player.updateDisplay(window);
@@ -32,3 +40,11 @@ int main()
 
 	return 0;
 }
+
+// bool collideWithWall(Image backGround, int x, int y)
+// {
+// 	Color wallColor(229, 153, 121); // orange
+// 	if (backGround.getPixel(x, y + PLAYER_SIZE) == wallColor)
+// 		return true;
+// 	return false;
+// }
