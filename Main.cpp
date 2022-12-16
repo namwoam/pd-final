a2d4c68497ef8a55095f5f0f47c9a0f3ec8f376c
student-Ian <65768926+student-Ian@users.noreply.github.com>
2022-12-16 23:03:21 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,10 @@
 #include <iostream>
 using namespace sf;
 
-bool collideWithWall(Image image, int x, int y);
+int collidesWithRest(Image image, int x, int y);
+int collidesWithWall(Image image, int x, int y);
+int collidesWithTrans(Image image, int x, int y);
+bool collidesWithCoin(Image image, int x, int y);
 
 int main()
 {
@@ -11,10 +14,10 @@ int main()
 	RenderWindow window(VideoMode(900, 900), "PD Final", Style::Titlebar | Style::Close);
 
 	Texture topLeft, topRight, bottomLeft, bottomRight;
-	topLeft.loadFromFile("C:/Users/olaba/Downloads/1671090937051.jpg");
-	topRight.loadFromFile("C:/Users/olaba/Downloads/1671090952623.jpg");
-	bottomLeft.loadFromFile("C:/Users/olaba/Downloads/1671090962853.jpg");
-	bottomRight.loadFromFile("C:/Users/olaba/Downloads/1671090974143.jpg");
+	topLeft.loadFromFile("C:/Users/olaba/Downloads/topLeft.png");
+	topRight.loadFromFile("C:/Users/olaba/Downloads/topRight.png");
+	bottomLeft.loadFromFile("C:/Users/olaba/Downloads/bottomLeft.png");
+	bottomRight.loadFromFile("C:/Users/olaba/Downloads/bottomRight.png");
 
 	Sprite* map[4];
 	map[0] = new Sprite(topLeft);
@@ -23,10 +26,10 @@ int main()
 	map[3] = new Sprite(bottomRight);
 
 	Image imgMap[4];
-	imgMap[0].loadFromFile("C:/Users/olaba/Downloads/1671090937051.jpg");
-	imgMap[1].loadFromFile("C:/Users/olaba/Downloads/1671090952623.jpg");
-	imgMap[2].loadFromFile("C:/Users/olaba/Downloads/1671090962853.jpg");
-	imgMap[3].loadFromFile("C:/Users/olaba/Downloads/1671090974143.jpg");
+	imgMap[0].loadFromFile("C:/Users/olaba/Downloads/topLeft.png");
+	imgMap[1].loadFromFile("C:/Users/olaba/Downloads/topRight.png");
+	imgMap[2].loadFromFile("C:/Users/olaba/Downloads/bottomLeft.png");
+	imgMap[3].loadFromFile("C:/Users/olaba/Downloads/bottomRight.png");
 
 	Event event;
 
@@ -42,7 +45,10 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 			if (event.type == Event::KeyPressed)
+			{
 				player.processEvents(event.key.code, true);
+				std::cout << "a key is being pressed";
+			}
 			if (event.type == Event::KeyReleased)
 				player.processEvents(event.key.code, false);
 		}
@@ -51,11 +57,103 @@ int main()
 		window.draw(*map[curBg]);
 		Time elapsed = clock.restart();
 		player.updateMotion(elapsed);
-		std::cout
-			<< (int)imgMap[curBg].getPixel(player.getPos().x, player.getPos().y).g
-			<< " ";
-		if (collideWithWall(imgMap[curBg], player.getPos().x, player.getPos().y))
-			player.stop();
+
+		if (player.timer > 0)
+			player.timer--;
+
+		switch (collidesWithRest(imgMap[curBg], player.getPos().x, player.getPos().y))
+		{
+			case 0:
+				break;
+
+			case 1:
+				player.yStop(false);
+				player.addFuel();
+				break;
+
+			case 2:
+				player.yStop(true);
+				break;
+
+			case 3:
+				player.xStop(false);
+				break;
+
+			case 4:
+				player.xStop(true);
+				break;
+
+			default:
+				break;
+		}
+
+		switch (collidesWithWall(imgMap[curBg], player.getPos().x, player.getPos().y))
+		{
+			case 0:
+				break;
+
+			case 1:
+				player.yStop(false);
+				break;
+
+			case 2:
+				player.yStop(true);
+				break;
+
+			case 3:
+				player.xStop(false);
+				break;
+
+			case 4:
+				player.xStop(true);
+				break;
+
+			default:
+				break;
+		}
+
+		switch (collidesWithTrans(imgMap[curBg], player.getPos().x, player.getPos().y))
+		{
+			case 0:
+				break;
+
+			case 1:
+				player.yStop(false); // should be removed
+
+				// shift y-coordinate to the top
+
+				break;
+
+			case 2:
+				player.yStop(true); // should be removed
+
+				// shift y - coordinate to the bottom
+
+				break;
+
+			case 3:
+				player.xStop(false); // should be removed
+
+				// shift x-coordinate to the right
+
+				break;
+
+			case 4:
+				player.xStop(true); // should be removed
+
+				// shift x-coordinate to the left
+
+				break;
+
+			default:
+				break;
+		}
+
+		if (collidesWithCoin(imgMap[curBg], player.getPos().x, player.getPos().y)) // might need a cooler
+		{
+			player.earn();
+		}
+
 		player.updateDisplay(window);
 		window.display();
 	}
@@ -63,9 +161,101 @@ int main()
 	return 0;
 }
 
-bool collideWithWall(Image backGround, int x, int y)
+int collidesWithRest(Image backGround, int x, int y)
 {
-	if (backGround.getPixel(x, y + PLAYER_SIZE).r > 200 && backGround.getPixel(x, y + PLAYER_SIZE).g < 200)
+	if (backGround.getPixel(x, y + PLAYER_SIZE).r > 180
+		&& backGround.getPixel(x, y + PLAYER_SIZE).g < 180
+		&& backGround.getPixel(x, y + PLAYER_SIZE).b < 150)
+		return 1;
+
+	if (backGround.getPixel(x, y - PLAYER_SIZE).r > 180
+		&& backGround.getPixel(x, y - PLAYER_SIZE).g < 180
+		&& backGround.getPixel(x, y - PLAYER_SIZE).b < 150)
+		return 2;
+
+	if (backGround.getPixel(x - PLAYER_SIZE, y).r > 180
+		&& backGround.getPixel(x - PLAYER_SIZE, y).g < 180
+		&& backGround.getPixel(x - PLAYER_SIZE, y).b < 150)
+		return 3;
+
+	if (backGround.getPixel(x + PLAYER_SIZE, y).r > 180
+		&& backGround.getPixel(x + PLAYER_SIZE, y).g < 180
+		&& backGround.getPixel(x + PLAYER_SIZE, y).b < 150)
+		return 4;
+
+	return 0;
+}
+
+int collidesWithWall(Image backGround, int x, int y)
+{
+	if (backGround.getPixel(x, y + PLAYER_SIZE).r > 220
+		&& backGround.getPixel(x, y + PLAYER_SIZE).g > 220
+		&& backGround.getPixel(x, y + PLAYER_SIZE).b > 220)
+		return 1;
+
+	if (backGround.getPixel(x, y - PLAYER_SIZE).r > 220
+		&& backGround.getPixel(x, y - PLAYER_SIZE).g > 220
+		&& backGround.getPixel(x, y - PLAYER_SIZE).b > 220)
+		return 2;
+
+	if (backGround.getPixel(x - PLAYER_SIZE, y).r > 220
+		&& backGround.getPixel(x - PLAYER_SIZE, y).g > 220
+		&& backGround.getPixel(x - PLAYER_SIZE, y).b > 220)
+		return 3;
+
+	if (backGround.getPixel(x + PLAYER_SIZE, y).r > 220
+		&& backGround.getPixel(x + PLAYER_SIZE, y).g > 220
+		&& backGround.getPixel(x + PLAYER_SIZE, y).b > 220)
+		return 4;
+
+	return 0;
+}
+
+int collidesWithTrans(Image backGround, int x, int y)
+{
+	if (backGround.getPixel(x, y + PLAYER_SIZE).r > 150
+		&& backGround.getPixel(x, y + PLAYER_SIZE).g > 200
+		&& backGround.getPixel(x, y + PLAYER_SIZE).b > 160)
+		return 1;
+
+	if (backGround.getPixel(x, y - PLAYER_SIZE).r > 150
+		&& backGround.getPixel(x, y - PLAYER_SIZE).g > 200
+		&& backGround.getPixel(x, y - PLAYER_SIZE).b > 160)
+		return 2;
+
+	if (backGround.getPixel(x - PLAYER_SIZE, y).r > 150
+		&& backGround.getPixel(x - PLAYER_SIZE, y).g > 200
+		&& backGround.getPixel(x - PLAYER_SIZE, y).b > 160)
+		return 3;
+
+	if (backGround.getPixel(x + PLAYER_SIZE, y).r > 150
+		&& backGround.getPixel(x + PLAYER_SIZE, y).g > 200
+		&& backGround.getPixel(x + PLAYER_SIZE, y).b > 160)
+		return 4;
+
+	return 0;
+}
+
+bool collidesWithCoin(Image backGround, int x, int y)
+{
+	if (backGround.getPixel(x, y + PLAYER_SIZE).r > 220
+		&& backGround.getPixel(x, y + PLAYER_SIZE).g > 180
+		&& backGround.getPixel(x, y + PLAYER_SIZE).b < 50)
+		return true;
+
+	if (backGround.getPixel(x, y - PLAYER_SIZE).r > 220
+		&& backGround.getPixel(x, y - PLAYER_SIZE).g > 180
+		&& backGround.getPixel(x, y - PLAYER_SIZE).b < 50)
+		return true;
+
+	if (backGround.getPixel(x - PLAYER_SIZE, y).r > 220
+		&& backGround.getPixel(x - PLAYER_SIZE, y).g > 180
+		&& backGround.getPixel(x - PLAYER_SIZE, y).b < 50)
+		return true;
+
+	if (backGround.getPixel(x + PLAYER_SIZE, y).r > 220
+		&& backGround.getPixel(x + PLAYER_SIZE, y).g > 180
+		&& backGround.getPixel(x + PLAYER_SIZE, y).b < 50)
 		return true;
 	return false;
 }
